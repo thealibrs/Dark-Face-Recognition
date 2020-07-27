d713ddb005b3af574dab331856c570bc340a1b8e
Ali Barış Ayten <alibrs15@hotmail.com>
2020-07-27 13:15:28 +0300

    Update face_recog.py

--- face_recog.py
+++ face_recog.py
@@ -2,14 +2,6 @@ import face_recognition
 import cv2
 import numpy as np
 
-# This is a demo of running face recognition on live video from your webcam. It's a little more complicated than the
-# other example, but it includes some basic performance tweaks to make things run a lot faster:
-#   1. Process each video frame at 1/4 resolution (though still display it at full resolution)
-#   2. Only detect faces in every other frame of video.
-
-# PLEASE NOTE: This example requires OpenCV (the `cv2` library) to be installed only to read from your webcam.
-# OpenCV is *not* required to use the face_recognition library. It's only required if you want to run this
-# specific demo. If you have trouble installing it, try any of the other demos that don't require it instead.
 
 # Get a reference to webcam #0 (the default one)
 video_capture = cv2.VideoCapture(0)
@@ -75,12 +67,6 @@ while True:
             if True in matches:
                 first_match_index = matches.index(True)
                 name = known_face_names[first_match_index]
-
-            # Or instead, use the known face with the smallest distance to the new face
-            #face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
-            #best_match_index = np.argmin(face_distances)
-            #if matches[best_match_index]:
-            #    name = known_face_names[best_match_index]
 
             face_names.append(name)
 
